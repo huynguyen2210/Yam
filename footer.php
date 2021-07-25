240300d9a94d403cb45ea3af2942b274722566ee
huynguyen2210 <huynguyen2210@gmail.com>
2021-07-25 12:31:09 +0700

    update

---
+++ footer.php
@@ -0,0 +1,70 @@
+<footer>
+    <div class="container">
+        <div class="footer-main d-lg-flex row">
+            <a class="footer-logo col-xl-1 col-lg-2 col-12 mt-0" href="#"><img src="/images/logo-footer.png" alt=""></a>
+            <div class="footer-text col-lg-10 col-12 d-sm-flex">
+                <div class="d-flex justify-content-between mt-3 mt-sm-0">
+                    <div class="footer-text-col col-6 mr-lg-2" id="footer--about">
+                        <div class="footer-text-col-title">About</div>
+                        <ul>
+                            <li><a href="">Story</a></li>
+                            <li><a href="">ME Group</a></li>
+                            <li><a href="">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-text-col col-6 mr-lg-2" id="footer--about">
+                        <div class="footer-text-col-title">Creators</div>
+                        <ul>
+                            <li><a href="">ME Group</a></li>
+                            <li><a href="">Lastest Content</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between mt-3 mt-sm-0">
+                    <div class="footer-text-col col-6 mr-lg-2" id="footer--about">
+                        <div class="footer-text-col-title">Solutions</div>
+                        <ul>
+                            <li><a href="">Music</a></li>
+                            <li><a href="">Health & Well-being</a></li>
+                            <li><a href="">Sports</a></li>
+                            <li><a href="">Gaming</a></li>
+                            <li><a href="">Travel</a></li>
+                            <li><a href="">Lifestyle</a></li>
+                            <li><a href="">Entertainment</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-text-col col-6 mr-lg-2" id="footer--about">
+                        <div class="footer-text-col-title">Product</div>
+                        <ul>
+                            <li><a href="">For Creator</a></li>
+                            <li><a href="">For Fans</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer-text-col mt-3 mt-sm-0" id="footer--about">
+                    <div class="footer-text-col-title">Contact Us</div>
+                    <ul>
+                        <li><button class="btn btn-main-yam px-3"><a href="#">Get Started</a></button></li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">T&C</a></li>
+                        <li><a href="">Policies & Guidelines</a></li>
+                        <li class="footer-item my-auto">
+                            <span class="footer-item-dot"></span>
+                            <span class="footer-item-dot"></span>
+                            <span class="footer-item-dot"></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="main.js"></script>
+</body>
+
+</html>
