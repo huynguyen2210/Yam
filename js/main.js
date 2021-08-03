2e6a70b6b8386715a5b0e58bd5c50a12c6ff6475
huynguyen2210 <huynguyen2210@gmail.com>
2021-08-03 17:49:46 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.5.0.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
 $(document).ready(function() {
     //Show header when scroll down
     var scrollPos = 0;
